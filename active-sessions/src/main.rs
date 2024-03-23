04a1cfbe533bb3af7820c9271f817f2f57cf421d
wklepacz <wiktorklepacz@gmail.com>
2024-03-23 12:03:37 +0100

    Preliminary code for adding user to the active sessions data base

--- active-sessions/src/main.rs
+++ active-sessions/src/main.rs
@@ -1,45 +1,63 @@
 use tonic::{transport::Server, Request, Response, Status};
+use async_trait::async_trait;
+use redis::AsyncCommands;
+use active_sessions::{UserData, ActiveSessions};
 
-use std::env;
-use tokio_postgres::NoTls;
-use dotenv::dotenv;
+pub mod active_sessions {
+    tonic::include_proto!("active_sessions");
+}
+
+
+#[derive(Default)]
+pub struct ActiveSessionsService;
+
+#[async_trait]
+impl ActiveSessions for ActiveSessionsService {
+    async fn add_user(
+        &self,
+        request: Request<UserData>,             //Userdata from auth-service
+    )-> Result<Response<String>, Status> {      
+
+        let user_data = request.into_inner();
+
+        let session_token = generate_session_token();
+        
+        let redis_url = "redis://localhost:6379/"; 
+        let redis_client = redis::Client::open("redis_url").unwrap();
+        let mut redis_con = redis_client.get_async_connection().await.unwrap();
+
+        let _: () = redis_con.hset_multiple(
+            &user_data.username,
+            &[
+                ("email", &user_data.email),
+                ("key", &user_data.key),
+                ("locaion", &user_data.location),
+                ("session_token", &session_token),
+            ]
+        ).await.unwrap();
+        
+        //Returns token generated for the user 
+        Ok(Response::new(session_token))
+    }
+}
+
+
+fn generate_session_token() -> String {
+    //random token generation 
+    use rand::distributions::Alphanumeric;
+    use rand::{thread_rng, Rng};
+    thread_rng().sample_iter(&Alphanumeric).take(30).collect()
+}
 
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let database_url = "postgres://postgres:mysecretpassword@auth-service-db/postgres";
+    let addr = "[::1]:50051".parse()?;
+    let active_sessions_service = ActiveSessionsService::default();
 
-    let (client, connection) =
-        tokio_postgres::connect(&database_url, NoTls).await?;
-
-    tokio::spawn(async move {
-        if let Err(e) = connection.await {
-            eprintln!("connection error: {}", e);
-        }
-    });
-
-    let table_creation_query = r#"
-        CREATE TABLE IF NOT EXISTS users (
-            Id SERIAL PRIMARY KEY,
-            email VARCHAR(255) NOT NULL UNIQUE,
-            username VARCHAR(50) NOT NULL UNIQUE,
-            hashed_password TEXT NOT NULL,
-            first_name VARCHAR(100),
-            last_name VARCHAR(100)
-    )"#;
-
-    client
-        .execute(table_creation_query, &[])
+    Server::builder()
+        .add_service(ActiveSessionsServer::new(active_sessions_service))
+        .serve(addr)
         .await?;
-
-    let addUserQuery = r#"
-        INSERT INTO users (email, username, hashed_password, first_name, last_name)
-        VALUES ('brud@brud.pl', 'brud', '8rud!', 'Brudas', 'Brudowski')
-    "#;
-
-    client
-        .execute(addUserQuery, &[]).await?;
-    
-    
     Ok(())
 }
