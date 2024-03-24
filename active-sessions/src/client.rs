670c7dcf9f338165a27e0a173d2fa580202f7dea
wklepacz <wiktorklepacz@gmail.com>
2024-03-24 16:35:07 +0100

    Added client.rs file for testing, ultimately it should be in auth service

---
+++ active-sessions/src/client.rs
@@ -0,0 +1,30 @@
+
+use active_sessions::{UserData, UserDataResponse, ActiveSessions};
+
+pub mod active_sessions {
+    tonic::include_proto!("active-sessions");
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let channel = Channel::from_static("http://[::1]:50051")
+        .connect()
+        .await?;
+
+    let mut client = ActiveSessionsClient::new(channel);
+
+    let request = tonic::Request::new(
+        UserData{
+            username: "Wiktor".to_owned(),
+            email: "abc@gmail.com".to_owned(),
+            key: "1234".to_owned(),
+            location: "Warsaw".to_owned(); 
+        }
+    );
+
+    let resposne = client.add_user(request).await?; 
+
+    println!("RESPONSE = {:?}", response); 
+
+    Ok(())
+}
