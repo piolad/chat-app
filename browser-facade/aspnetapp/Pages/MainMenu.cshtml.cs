9c29509dbc17140c1011a17c21feeedf4a755642
jlaz <01178193@pw.edu.pl>
2024-04-21 14:10:15 +0200

    Chnaged Main-service and browser-facade so that they can communicate between each other, simplified version not using the right proto file

--- browser-facade/aspnetapp/Pages/MainMenu.cshtml.cs
+++ browser-facade/aspnetapp/Pages/MainMenu.cshtml.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using Grpc.Net.Client;
+using Greeter; // Import the correct namespace
 
 namespace aspnetapp.Pages
 {
@@ -21,13 +23,36 @@ namespace aspnetapp.Pages
 
         public IActionResult OnPost()
         {
-            _logger.LogInformation("Login submitted with Username: {Username}, Password: {Password}", Username, Password);
-            
-            if(Username != "admin" || Password != "admin")
+            try
             {
-                ViewData["AlertMessage"] = "Invalid username or password. Please try again.";;
-            }else{
-                ViewData["AlertMessage"] = "Good work! Keep it up";
+                _logger.LogInformation("Login submitted with Username: {Username}, Password: {Password}", Username, Password);
+                
+                if(Username != "admin" || Password != "admin")
+                {
+                    ViewData["AlertMessage"] = "Invalid username or password. Please try again.";
+                }
+                else
+                {
+                    // Create an insecure gRPC channel
+                    using var channel = GrpcChannel.ForAddress("http://main-service:50050");
+                    
+                    // Create a gRPC client
+                    var client = new Greeter.Greeter.GreeterClient(channel);
+                    
+                    // Create a request message
+                    var request = new HelloRequest { Name = Username };
+                    
+                    // Call the gRPC service
+                    var response = client.SayHello(request);
+                    
+                    // Display the response message
+                    ViewData["AlertMessage"] = response.Message;
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An error occurred while processing the gRPC request.");
+                ViewData["AlertMessage"] = "An error occurred while processing your request. Please try again later.";
             }
 
             // Refresh the page to display the alert message
--- main-service/src/server.js
+++ main-service/src/server.js
@@ -1,17 +1,15 @@
 const grpc = require('@grpc/grpc-js');
 const protoLoader = require('@grpc/proto-loader');
 
-const packageDefinition = protoLoader.loadSync('greet.proto', {});
-const greeterProto = grpc.loadPackageDefinition(packageDefinition).greeter;
+const packageDefinition = protoLoader.loadSync('../protos/Greeter.proto', {});
+const greeterProto = grpc.loadPackageDefinition(packageDefinition).Greeter;
 
 function sayHello(call, callback) {
   callback(null, { message: 'Hello ' + call.request.name });
 }
 
 const server = new grpc.Server();
-server.addService(greeterProto.Greeter.service, { sayHello: sayHello });
-server.bindAsync('0.0.0.0:50051', grpc.ServerCredentials.createInsecure(), () => {
-
-  
-  console.log('Server running at http://0.0.0.0:50051');
+server.addService(greeterProto.Greeter.service, { SayHello: sayHello });
+server.bindAsync('0.0.0.0:50050', grpc.ServerCredentials.createInsecure(), () => {
+  console.log('Server running at 0.0.0.0:50050');
 });
