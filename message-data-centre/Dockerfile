fe2f0098a1c1384c2ee60c1c8cf8f121ce037821
jlaz <01178193@pw.edu.pl>
2024-05-19 21:49:17 +0200

    starting a golang program

---
+++ message-data-centre/Dockerfile
@@ -0,0 +1,10 @@
+FROM golang:latest
+
+WORKDIR /go/src/app
+
+COPY . .
+
+RUN go get -d -v ./...
+RUN go install -v ./...
+
+CMD ["app"]
---
+++ message-data-centre/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Witaj, świecie! Jestem aplikacją Go w folderze message-data-center.")
+}
