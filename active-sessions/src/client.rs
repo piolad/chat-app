d0b9cd6a4a1d943e0ceebd9568163f9d84302d74
Wiktor Klepacz <wiktorklepacz@gmail.com>
2024-05-19 19:00:55 +0200

    bug fixes

--- active-sessions/src/client.rs
+++ active-sessions/src/client.rs
@@ -1,32 +1,35 @@
 use tonic::{transport::Channel, Request, Response, Status};
 
 pub mod active_sessions {
-    tonic::include_proto!("active-sessions");
+    tonic::include_proto!("active_sessions");
 }
+
+use active_sessions::{UserData, UserDataResponse};
+use active_sessions::active_sessions_client::ActiveSessionsClient;
 
 use active_sessions::{UserData, UserDataResponse, ActiveSessions};
 
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let channel = Channel::from_static("http://[::1]:50052")
+    let channel = Channel::from_static("http://127.0.0.1:50052")
         .connect()
         .await?;
 
     let mut client = ActiveSessionsClient::new(channel);
 
     let request = tonic::Request::new(
-        UserData{
+        UserData {
             username: "Wiktor".to_owned(),
             email: "abc@gmail.com".to_owned(),
             key: "1234".to_owned(),
-            location: "Warsaw".to_owned(), 
+            location: "Warsaw".to_owned(),
         }
     );
 
-    let response = client.add_user(request).await?; 
+    let response = client.add_user(request).await?;
 
-    println!("RESPONSE = {:?}", response); 
+    println!("RESPONSE = {:?}", response);
 
     Ok(())
 }
--- active-sessions/src/main.rs
+++ active-sessions/src/main.rs
@@ -1,44 +1,38 @@
 use tonic::{transport::Server, Request, Response, Status};
-
 use rand::distributions::Alphanumeric;
 use rand::{thread_rng, Rng};
-
 use async_trait::async_trait;
 use redis::AsyncCommands;
 
 pub mod active_sessions {
-    tonic::include_proto!("active-sessions");
+    tonic::include_proto!("active_sessions");
 }
 
-use active_sessions::{UserData, UserDataResponse, ActiveSessions};
-
+use active_sessions::{UserData, UserDataResponse};
+use active_sessions::active_sessions_server::{ActiveSessions, ActiveSessionsServer};
 
 #[derive(Default)]
 pub struct ActiveSessionsService;
 
 #[async_trait]
-impl ActiveSessions for ActiveSessionsService {
+impl ActiveSessions for ActiveSessionsService{
     async fn add_user(
         &self,
-        request: Request<UserData>,             //Userdata from auth-service
-    )-> Result<Response<String>, Status> {      
-
+        request: Request<UserData>,
+    ) -> Result<Response<UserDataResponse>, Status> {
         let user_data = request.into_inner();
-
         let session_token = generate_session_token();
         
         let redis_url = "redis://redis:6379/";
-        let redis_client = redis::Client::open(redis_url)
-            .map_err(|e| {
-                eprintln!("Failed to connect to Redis: {:?}", e);
-                Status::internal("Failed to connect to Redis")
-            })?;
+        let redis_client = redis::Client::open(redis_url).map_err(|e| {
+            eprintln!("Failed to connect to Redis: {:?}", e);
+            Status::internal("Failed to connect to Redis")
+        })?;
 
-        let mut redis_con = redis_client.get_async_connection().await
-            .map_err(|e| {
-                eprintln!("Failed to get Redis connection: {:?}", e);
-                Status::internal("Failed to get Redis connection")
-            })?;
+        let mut redis_con = redis_client.get_async_connection().await.map_err(|e| {
+            eprintln!("Failed to get Redis connection: {:?}", e);
+            Status::internal("Failed to get Redis connection")
+        })?;
 
         let _: () = redis_con.hset_multiple(
             &user_data.username,
@@ -49,37 +43,36 @@ impl ActiveSessions for ActiveSessionsService {
                 ("location", &user_data.location),
                 ("session_token", &session_token),
             ]
-        ).await 
-            .map_err(|e| {
-                eprintln!("Failed to set data in Redis: {:?}", e);
-                Status::internal("Failed to set data in Redis")
-            })?;
-        
-        // this is done to correctly format the gRPC response
-        let response = UserDataResponse{
+        ).await.map_err(|e| {
+            eprintln!("Failed to set data in Redis: {:?}", e);
+            Status::internal("Failed to set data in Redis")
+        })?;
+
+        let response = UserDataResponse {
             session_token: session_token.clone(),
-        }; 
+        };
 
         Ok(Response::new(response))
-    } 
+    }
 }
-
-
 
 fn generate_session_token() -> String {
-    //random token generation 
-    thread_rng().sample_iter(&Alphanumeric).take(30).collect()
+    thread_rng()
+        .sample_iter(&Alphanumeric)
+        .take(30)
+        .map(char::from)
+        .collect()
 }
-
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let addr = "[::1]:50052".parse()?;
+    let addr = "127.0.0.1:50052".parse()?;
     let active_sessions_service = ActiveSessionsService::default();
 
     Server::builder()
         .add_service(ActiveSessionsService::new(active_sessions_service))
         .serve(addr)
         .await?;
+
     Ok(())
 }
