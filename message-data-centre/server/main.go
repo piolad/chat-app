91d0bf2369b075f79230e5e9d6632098cf467b9d
jlaz <01178193@pw.edu.pl>
2024-06-01 21:01:31 +0200

    Now i can insert messages into database

--- message-data-centre/server/main.go
+++ message-data-centre/server/main.go
@@ -4,14 +4,21 @@ import (
 	"context"
 	"log"
 	"net"
+	"time"
 
 	pb "message-data-centre/proto"
 
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"google.golang.org/grpc"
 )
 
 const (
-	port = ":50051"
+	port                    = ":50051"
+	mongoDBConnectionString = "mongodb://adminUser:adminPassword@message-data-centre-db:27017"
+	mongoDBName             = "message-db"
+	mongoCollectionName     = "messages"
 )
 
 // server is used to implement service.GreeterServer.
@@ -21,10 +28,75 @@ type server struct {
 
 // SayHello implements service.GreeterServer
 func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
+	s.SaveMessage(ctx, in)
 	return &pb.HelloReply{Message: "Hello " + in.GetName()}, nil
 }
 
+// SaveMessage implements service.GreeterServer
+func (s *server) SaveMessage(ctx context.Context, in *pb.HelloRequest) (*pb.HelloRequest, error) {
+	client, err := mongo.Connect(context.Background(), options.Client().ApplyURI(mongoDBConnectionString))
+	if err != nil {
+		return nil, err
+	}
+	defer client.Disconnect(context.Background())
+
+	collection := client.Database(mongoDBName).Collection(mongoCollectionName)
+
+	// Create a BSON document from the input message
+	messageDocument := bson.M{
+		"message":   in.GetName(), // Accessing the "name" field from HelloRequest
+		"timestamp": time.Now(),
+		"status":    "unread",
+	}
+
+	// Insert the document into the collection
+	_, err = collection.InsertOne(context.Background(), messageDocument)
+	if err != nil {
+		return nil, err
+	}
+
+	return in, nil
+}
+
+func ensureCollectionExists() error {
+	client, err := mongo.Connect(context.Background(), options.Client().ApplyURI(mongoDBConnectionString))
+	if err != nil {
+		return err
+	}
+	defer client.Disconnect(context.Background())
+
+	collection := client.Database(mongoDBName).Collection(mongoCollectionName)
+
+	// Define the index keys
+	indexKeys := bson.D{
+		{"message", 1},
+		{"timestamp", 1},
+		{"status", 1},
+	}
+
+	// Define the index options
+	indexOptions := options.Index().SetUnique(true)
+
+	// Create the index model
+	indexModel := mongo.IndexModel{
+		Keys:    indexKeys,
+		Options: indexOptions,
+	}
+
+	// Create the index
+	_, err = collection.Indexes().CreateOne(context.Background(), indexModel)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
 func main() {
+	if err := ensureCollectionExists(); err != nil {
+		log.Fatalf("failed to ensure collection exists: %v", err)
+	}
+
 	lis, err := net.Listen("tcp", port)
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
