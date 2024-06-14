59ccd0347f359a9b9fd7b8656cfe7fc615193e3b
jlaz <01178193@pw.edu.pl>
2024-06-14 19:38:32 +0200

    I have created a possiblity to save messages and create conversations when the first message exists,

--- message-data-centre/server/main.go
+++ message-data-centre/server/main.go
@@ -8,9 +8,6 @@ import (
 
 	pb "message-data-centre/proto"
 
-	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
 	"google.golang.org/grpc"
 )
 
@@ -25,70 +22,22 @@ type server struct {
 	pb.UnimplementedGreeterServer
 }
 
+type message struct {
+	Message        string `bson:"message" json:"message"`
+	Timestamp      string `bson:"timestamp" json:"timestamp"`
+	ConversationID string `bson:"conversation_id" json:"conversation_id"`
+}
+
 // SayHello implements service.GreeterServer
 func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
-	s.SaveMessage(ctx, in)
+	message := &message{
+		Message:        "Hello " + in.GetName(),
+		Timestamp:      time.Now().String(),
+		ConversationID: "0",
+	}
+
+	s.SaveMessage(ctx, message)
 	return &pb.HelloReply{Message: "Hello " + in.GetName()}, nil
-}
-
-// SaveMessage implements service.GreeterServer
-func (s *server) SaveMessage(ctx context.Context, in *pb.HelloRequest) (*pb.HelloRequest, error) {
-	client, err := mongo.Connect(context.Background(), options.Client().ApplyURI(mongoDBConnectionString))
-	if err != nil {
-		return nil, err
-	}
-	defer client.Disconnect(context.Background())
-
-	collection := client.Database(mongoDBName).Collection("messages")
-
-	// Create a BSON document from the input message
-	messageDocument := bson.M{
-		"message":   in.GetName(), // Accessing the "name" field from HelloRequest
-		"timestamp": time.Now(),
-		"status":    "unread",
-	}
-
-	// Insert the document into the collection
-	_, err = collection.InsertOne(context.Background(), messageDocument)
-	if err != nil {
-		return nil, err
-	}
-
-	return in, nil
-}
-
-func ensureCollectionExists_Messages() error {
-	client, err := mongo.Connect(context.Background(), options.Client().ApplyURI(mongoDBConnectionString))
-	if err != nil {
-		return err
-	}
-	defer client.Disconnect(context.Background())
-
-	collection := client.Database(mongoDBName).Collection("messages")
-
-	// Define the index keys
-	indexKeys := bson.D{
-		{Key: "message", Value: 1},
-		{Key: "timestamp", Value: -1},
-		{Key: "status", Value: 1},
-	}
-
-	// Define the index options
-	indexOptions := options.Index().SetUnique(true)
-
-	// Create the index model
-	indexModel := mongo.IndexModel{
-		Keys:    indexKeys,
-		Options: indexOptions,
-	}
-
-	// Create the index
-	_, err = collection.Indexes().CreateOne(context.Background(), indexModel)
-	if err != nil {
-		return err
-	}
-
-	return nil
 }
 
 func main() {
--- message-data-centre/server/messages.go
+++ message-data-centre/server/messages.go
@@ -2,90 +2,54 @@ package main
 
 import (
 	"context"
-	"log"
-	pb "message-data-centre/proto"
-	"time"
 
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-const (
-	mongoConversationCollectionName = "Conversations"
-	mongoMessageCollectionName      = "Messages"
-)
-
-func (s *server) SendMessage(ctx context.Context, in *pb.Message) (*pb.Response, error) {
-	log.Printf("Recived message: %v", in.GetMessage())
-	s.ensureConversationExists(in.GetSender(), in.GetReceiver())
-	return &pb.Response{Message: "Message send by " + in.GetSender() + " to " + in.GetReceiver() + " with message : " + in.GetMessage() + " at time " + in.GetTimestamp()}, nil
-}
-
-func (s *server) ensureConversationExists(sender string, receiver string) error {
+// Saves a message to the database from the conversation service
+func (s *server) SaveMessage(ctx context.Context, in *message) error {
 	client, err := mongo.Connect(context.Background(), options.Client().ApplyURI(mongoDBConnectionString))
 	if err != nil {
-		log.Fatal(err) //error during connection to
 		return err
 	}
 	defer client.Disconnect(context.Background())
 
-	collection := client.Database(mongoDBName).Collection("Conversations")
+	collection := client.Database(mongoDBName).Collection("messages")
 
-	// Define the filter to check for existing conversation in both directions
-	filter := bson.M{
-		"$or": []bson.M{
-			{"sender": sender, "receiver": receiver},
-			{"sender": receiver, "receiver": sender},
-		},
+	// Create a BSON document from the input message
+	messageDocument := bson.M{
+		"message":         in.Message,
+		"timestamp":       in.Timestamp,
+		"status":          "unread",
+		"conversation_id": in.ConversationID,
 	}
 
-	var result bson.M
-	err = collection.FindOne(context.Background(), filter).Decode(&result)
-	if err == mongo.ErrNoDocuments {
-		newConversation := bson.M{
-			"sender":         sender,
-			"receiver":       receiver,
-			"last_timestamp": time.Now(),
-			"iv_vector":      "",
-		}
-
-		_, err = collection.InsertOne(context.Background(), newConversation)
-		if err != nil {
-			log.Fatal(err)
-			return err
-		}
-
-		log.Println("New conversation created between sender:", sender, "and receiver:", receiver)
-		return nil
-	} else if err != nil {
-		// Handle other potential errors
-		log.Fatal(err)
+	// Insert the document into the collection
+	_, err = collection.InsertOne(context.Background(), messageDocument)
+	if err != nil {
 		return err
 	}
 
-	// If a document is found, log that the conversation already exists
-	log.Println("Conversation already exists between sender:", sender, "and receiver:", receiver)
 	return nil
 }
 
-func ensureCollectionExists_Conversations() error {
+func ensureCollectionExists_Messages() error {
 	client, err := mongo.Connect(context.Background(), options.Client().ApplyURI(mongoDBConnectionString))
 	if err != nil {
-		log.Fatal(err) //error during connection to database
 		return err
 	}
 	defer client.Disconnect(context.Background())
 
-	collection := client.Database(mongoDBName).Collection("Conversations")
+	collection := client.Database(mongoDBName).Collection("messages")
 
-	//Define the index key
+	// Define the index keys
 	indexKeys := bson.D{
-		{Key: "id", Value: 1},
-		{Key: "sender", Value: 1},
-		{Key: "receiver", Value: 1},
-		{Key: "last_timestamp", Value: 1},
-		{Key: "iv_vector", Value: 1},
+		{Key: "message", Value: 1},
+		{Key: "timestamp", Value: -1},
+		{Key: "status", Value: 1},
+		{Key: "conversation_id", Value: 1},
 	}
 
 	// Define the index options
