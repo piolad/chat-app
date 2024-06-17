de9c255b0721f6847b2ae81bc9f74266a6d10d13
jlaz <01178193@pw.edu.pl>
2024-06-17 15:22:31 +0200

    Now it is much faster

--- auth-service/src/security.rs
+++ auth-service/src/security.rs
@@ -4,7 +4,7 @@ use rand::rngs::OsRng;
 // Function to generate RSA keypair
 pub fn generate_rsa_keypair() -> (RsaPrivateKey, RsaPublicKey) {
     let mut rng = OsRng;
-    let bits = 2048; // Choose an appropriate key size
+    let bits = 256; // The smaller the value, the faster the key generation
     let private_key = RsaPrivateKey::new(&mut rng, bits).expect("Failed to generate private key");
     let public_key = RsaPublicKey::from(&private_key);
 
