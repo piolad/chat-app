98d1aa4c9b1a8c5e8454bcd31c58bc9623f0715c
jlaz <01178193@pw.edu.pl>
2024-08-23 16:16:42 +0200

    maybe is working

---
+++ browser-facade/aspnetapp/Pages/MainLogin.cshtml.cs
@@ -0,0 +1,47 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using Grpc.Net.Client;
+using Grpc.Core;
+using BrowserFacade;
+
+namespace aspnetapp.Pages
+{
+    public class MainLoginModel : PageModel
+    {
+        private readonly ILogger<MainLoginModel> _logger;
+
+        public MainLoginModel(ILogger<MainLoginModel> logger)
+        {
+            _logger = logger;
+        }
+            public string sender = "kuba";
+            public string receiver = "mati";
+            public string message = "Ja jestem najlepszy";
+            public string timestamp = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+
+        public IActionResult OnPost()
+        {
+            var callOptions = new CallOptions()
+                .WithDeadline(DateTime.UtcNow.AddSeconds(10)); // 10-second timeout
+
+            try
+            {             
+                using var channel = GrpcChannel.ForAddress("http://main-service:50050");
+                var client = new BrowserFacade.BrowserFacade.BrowserFacadeClient(channel);
+                var request = new Message { Sender = sender, Receiver = receiver, Message_ = message, Timestamp = timestamp}; //zmiana
+
+                var response = client.SendMessage(request, callOptions);
+
+                _logger.LogInformation("Message response: {Response}", response);
+            }
+            catch (RpcException ex)
+            {
+                _logger.LogError($"Error code: {ex.StatusCode}. Message: {ex.Status.Detail}");
+                ViewData["AlertMessage"] = $"Error code: {ex.StatusCode}. Message: {ex.Status.Detail}";
+            }       
+            return Page();
+        }
+    }
+}
