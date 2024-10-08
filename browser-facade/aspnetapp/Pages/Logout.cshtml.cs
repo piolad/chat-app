e1db102d2f589794749d1ab6136d5a4e2164ea9c
Piotr Ladkowski <piolad2003@gmail.com>
2024-10-08 22:28:12 +0200

    add logout functionality

---
+++ browser-facade/aspnetapp/Pages/Logout.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Authentication.Cookies;
+
+public class LogoutModel : PageModel
+{
+    public async Task<IActionResult> OnGet()
+    {
+        await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+        return RedirectToPage("/Index"); // Redirect to home or login after logout
+    }
+}
