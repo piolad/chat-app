dea8e179ecc14371d67d7c5012bf32d5477f7531
Piotr Łądkowski <piolad2003@gmail.com>
2025-11-03 10:44:40 +0100

    small cleanup + remove code present only on a single subpage

--- browser-facade/aspnetapp/Program.cs
+++ browser-facade/aspnetapp/Program.cs
@@ -21,10 +21,6 @@ builder.Services.AddSession(options =>
     options.IdleTimeout = TimeSpan.FromMinutes(30); // Set session timeout
 });
 
-// builder.Services.ConfigureHttpJsonOptions(options =>
-// {
-//     options.SerializerOptions.TypeInfoResolverChain.Insert(0, AppJsonSerializerContext.Default);
-// });
 
 var app = builder.Build();
 
